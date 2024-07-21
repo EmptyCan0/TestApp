244debe38a45d2b8462ab23251249b7a1343450c
emptycan01 <asadarikuto1004@outlook.jp>
2024-07-21 23:59:02 +0900

    logger　文法修正

--- testapp.py
+++ testapp.py
@@ -16,7 +16,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+logging.basicConfig(filename='logs/app.log', level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -27,17 +27,23 @@ def index():
 @app.route('/upload',methods=['POST'])
 def upload_file():
     print("start")
+    app.logger.info("upload開始")
     if 'file' not in request.files:
         print("1")
+        app.logger.error("ファイルがみつからない")
         return redirect(request.url)
     file = request.files['file']
     if file.filename == '':
         print("2")
+        app.logger.error("なまえが空")
         return redirect(request.url)
     if file and allowed_file(file.filename):
+        app.logger.info("ファイルのじょうほうOK ")
         filename = file.filename
         print(filename)
+        app.logger.info(filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        app.logger.info("保存 ")
         return f'File {filename} uploaded successfully!'
     else:
         return 'Invalid file type.'
