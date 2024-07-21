c4e05f73a32f673165f5c03f62c6e330cbb5c8d0
emptycan01 <asadarikuto1004@outlook.jp>
2024-07-21 15:59:08 +0900

    upload form is completed

--- testapp.py
+++ testapp.py
@@ -1,2 +1,43 @@
+from crypt import methods
+from flask import Flask, render_template, request, redirect
+import os
 
-print("hello world")
+app = Flask(__name__)
+
+# アップロードされたファイルを保存するディレクトリ
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 許可するファイル拡張子のリスト
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+	return render_template(
+        'index.html'
+    )
+
+@app.route('/upload',methods=['POST'])
+def upload_file():
+    print("start")
+    if 'file' not in request.files:
+        print("1")
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        print("2")
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        print(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return f'File {filename} uploaded successfully!'
+    else:
+        return 'Invalid file type.'
+
+if __name__ == "__main__":
+	app.run(debug=True)
