dc1bab53b08507d87ae8924d4c20fb86d748d7e8
emptycan01 <asadarikuto1004@outlook.jp>
2024-07-23 22:49:46 +0900

    logger 5rd type

--- testapp.py
+++ testapp.py
@@ -1,20 +1,66 @@
+from crypt import methods
+from flask import Flask, render_template, request, redirect
+import os
+
 import logging
 from logging.handlers import RotatingFileHandler
-from flask import Flask
 
 app = Flask(__name__)
 
-# ログ設定
-handler = RotatingFileHandler('/path/to/flask.log', maxBytes=10000, backupCount=1)
+# アップロードされたファイルを保存するディレクトリ
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 許可するファイル拡張子のリスト
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+handler = RotatingFileHandler('logs/app.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
 handler.setFormatter(formatter)
 app.logger.addHandler(handler)
 
 @app.route('/')
-def hello():
-    app.logger.info('Hello, world!')
-    return "Hello, World!"
+def index():
+    app.logger.info("upload開始")
+    return render_template(
+        'index.html'
+    )
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return f'File {filename} uploaded successfully!'
+
+@app.route('/upload',methods=['POST'])
+def upload_file():
+    print("start")
+    app.logger.info("upload開始")
+    if 'file' not in request.files:
+        print("1")
+        app.logger.error("ファイルがみつからない")
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        print("2")
+        app.logger.error("なまえが空")
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        app.logger.info("ファイルのじょうほうOK ")
+        filename = file.filename 
+        print(filename)
+        app.logger.info(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        app.logger.info("保存 ")
+        return f'File {filename} uploaded successfully!'
+    else:
+        return 'Invalid file type.'
+
+if __name__ == "__main__":
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(debug=True)
